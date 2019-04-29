9e77d101df164661265bc0a1b142bf54edb9b1d7
yuya1203 <yuyaaa.1203@gmail.com>
2019-04-30 02:05:19 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
