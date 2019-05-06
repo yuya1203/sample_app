803115ba5a3063819fdf7f34b0db7d6be738664b
yuya1203 <yuyaaa.1203@gmail.com>
2019-05-06 14:50:34 +0900

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :account_activations, only: [:edit]
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430134035) do
+ActiveRecord::Schema.define(version: 20190501074324) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20190430134035) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.time "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
